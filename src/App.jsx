c906deafffa8aebd8ad29f388bc23249bd5762f9
Tyler Potts <tylerpottsdev@gmail.com>
2022-04-28 10:38:56 +0100

    initial

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import QRCode from 'qrcode'
+import { useState } from 'react'
+
+function App() {
+	const [url, setUrl] = useState('')
+	const [qr, setQr] = useState('')
+
+	const GenerateQRCode = () => {
+		QRCode.toDataURL(url, {
+			width: 800,
+			margin: 2,
+			color: {
+				dark: '#335383FF',
+				light: '#EEEEEEFF'
+			}
+		}, (err, url) => {
+			if (err) return console.error(err)
+
+			console.log(url)
+			setQr(url)
+		})
+	}
+
+	return (
+		<div className="app">
+			<h1>QR Generator</h1>
+			<input 
+				type="text"
+				placeholder="e.g. https://google.com"
+				value={url}
+				onChange={e => setUrl(e.target.value)} />
+			<button onClick={GenerateQRCode}>Generate</button>
+			{qr && <>
+				<img src={qr} />
+				<a href={qr} download="qrcode.png">Download</a>
+			</>}
+		</div>
+	)
+}
+
+export default App
